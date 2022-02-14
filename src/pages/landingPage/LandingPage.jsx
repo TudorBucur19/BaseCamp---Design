c1354c784c0954316ac56b56256e27fa8f3c174b
TudorBucur19 <tudor.bucur88@gmail.com>
2022-02-14 13:43:06 +0200

    add overlay and restyle titles

--- src/pages/landingPage/LandingPage.jsx
+++ src/pages/landingPage/LandingPage.jsx
@@ -1,27 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import './LandingPage.scss';
-import { Typography, Button } from '@mui/material';
+
+import Button from 'components/Common/Button/Button';
+import { CampgroundsContext } from 'contexts/CampgroundsContext';
+import style from 'pages/LandingPage/LandingPage.module.scss';
 
 const LandingPage = () => {
+    const { campgroundsList } = useContext(CampgroundsContext);
+    console.log(campgroundsList)
+    const allCoverPhotos = campgroundsList.length && campgroundsList.map(item => item.campground.image);
+    const coverUrls = allCoverPhotos.length && allCoverPhotos.flat().slice(0, 5);
+    console.log(allCoverPhotos)
+    console.log(coverUrls)
+    const { landing, overlay, landing_header, landing_header_title, landing_header_subtitle, slideshow } = style;
 
     return ( 
-        <div className="landing">
-            <div className="landing__header">
-                <Typography component="h1" variant="h3" fontFamily="RocknRoll One" fontWeight="bold" mb={3}>Welcome to BaseCamp!</Typography>
+        <div className={landing}>
+            
+            <div className={landing_header}>
+                <h1 className={landing_header_title}>welcome to basecamp</h1>
+                <h2 className={landing_header_subtitle}>enjoy your camping stay</h2>
                 <Link to="/campgrounds">
-                    <Button variant="contained" color="secondary" size="large">View all campgrounds</Button>
+                    <Button variant="solid" label="campgrounds"/>
                 </Link>
                 
             </div>
-
-            <ul class="slideshow">
+            <div className={overlay}></div> 
+            <ul className={slideshow}>
                 <li></li>
                 <li></li>
                 <li></li>
                 <li></li>
                 <li></li>
             </ul>
+                       
         </div>
      );
 }
